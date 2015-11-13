c8d65f6404dafc8ebb91260731e370db9fab0d42
Ingo Lohmar <i.lohmar@gmail.com>
2015-11-13 13:40:51 +0100

    Fix --last-keyword
    - work for (< 1 (point-min)), fixes #7
    - always return nil (instead of keyword "") if there is not a single
      keyword before point

--- company-statistics.el
+++ company-statistics.el
@@ -184,12 +184,14 @@ number)."
 font-lock-keyword-face up to point, or nil."
   (let ((face-pos (point)))
     (while (and (number-or-marker-p face-pos)
-                (< 1 face-pos)
+                (< (point-min) face-pos)
                 (not (eq (get-text-property (1- face-pos) 'face)
                          'font-lock-keyword-face)))
       (setq face-pos
             (previous-single-property-change face-pos 'face nil (point-min))))
-    (when (and (number-or-marker-p face-pos))      ;else eval to nil
+    (when (and (number-or-marker-p face-pos)
+               (eq (get-text-property (max (point-min) (1- face-pos)) 'face)
+                   'font-lock-keyword-face))
       (list :keyword
             (buffer-substring-no-properties
              (previous-single-property-change face-pos 'face nil (point-min))
